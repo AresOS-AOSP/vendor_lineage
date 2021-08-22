62faca030720598c29cee16635c66ea12d26f7e6
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    vendor: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/ares.mk
+++ config/ares.mk
@@ -26,3 +26,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
